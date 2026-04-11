83c8f9bfac2d5c394942616b28ae34922a97d753
Niko0004Stud <niko0004@stud.ek.dk>
2026-04-11 13:39:05 +0200

    Opsætning

---
+++ src/main/java/org/gruppe3nskeskyfullstack/Gruppe3NskeskyFullstackApplication.java
@@ -0,0 +1,13 @@
+package org.gruppe3nskeskyfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe3NskeskyFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe3NskeskyFullstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gruppe3nskeskyfullstack/Gruppe3NskeskyFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gruppe3nskeskyfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe3NskeskyFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

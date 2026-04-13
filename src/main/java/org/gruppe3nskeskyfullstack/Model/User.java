7b18302595f684c24f727502ac5369a25c62cb3a
jshar <joha0013@stud.ek.dk>
2026-04-13 13:03:42 +0200

    begyndt structur for model user

---
+++ src/main/java/org/gruppe3nskeskyfullstack/Model/User.java
@@ -0,0 +1,16 @@
+package org.gruppe3nskeskyfullstack.Model;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String tlf_number;
+    private String gender;
+    private LocalDate birth_date;
+    private String password;
+
+}

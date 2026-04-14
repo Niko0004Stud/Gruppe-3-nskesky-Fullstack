7abed6587a8ef8ec5d327041760f4a0cae56ffbe
Estera Turcu <estu0002@stud.ek.dk>
2026-04-14 11:31:06 +0200

    input validering-brugernavn

--- src/main/java/org/gruppe3nskeskyfullstack/controller/ØnskelisteInputValidering.java
+++ src/main/java/org/gruppe3nskeskyfullstack/controller/ØnskelisteInputValidering.java
@@ -10,6 +10,7 @@ public class ØnskelisteInputValidering {
         if (name==null){
             throw new IllegalArgumentException("ønskelistenavn må ikke være null");
         }
+
         String trimmed=name.trim();
 
         if (trimmed.length()<3 || trimmed.length()>20){
@@ -17,7 +18,7 @@ public class ØnskelisteInputValidering {
         }
 
         if (!trimmed.matches("^[\\p{L}0-9.-_]+$")){
-            throw new IllegalArgumentException("øsnkelistenavn gyldige tegn er kun a-zA-Z0-9._-");
+            throw new IllegalArgumentException("øsnkelistenavn gyldige tegn er kun a-z A-Z 0-9 . _ -");
         }
 
     }

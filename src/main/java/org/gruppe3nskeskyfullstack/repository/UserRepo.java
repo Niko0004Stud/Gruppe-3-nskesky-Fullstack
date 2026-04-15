ac5b94f2cbb13293559a323ba13025e36b8c0d6d
Niko0004Stud <niko0004@stud.ek.dk>
2026-04-15 17:05:42 +0200

    Klasse UserRepo med verifyLogin(), der tjekker om brugeren med netop det password og email eksisterer, og opretter og returnere brugeren som objekt.

---
+++ src/main/java/org/gruppe3nskeskyfullstack/repository/UserRepo.java
@@ -0,0 +1,46 @@
+package org.gruppe3nskeskyfullstack.repository;
+
+import org.gruppe3nskeskyfullstack.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class UserRepo {
+
+    @Autowired
+    DataSource dataSource;
+    public User verifyLogin(String email, String password){
+        User user = null;
+        String sql = "SELECT * FROM users WHERE email = ?, password = ?";
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)){
+
+            try(ResultSet resultSet = statement.executeQuery()){
+                if(resultSet != null ){
+                    user = new User();
+                    user.setId(resultSet.getInt("id"));
+                    user.setFirstName(resultSet.getString("firstName"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setEmail(resultSet.getString("email"));
+                    user.setTlfNumber(resultSet.getString("tlfNumber"));
+                    user.setGender(resultSet.getString("gender"));
+                    user.setBirthDate(resultSet.getDate("birthDate").toLocalDate());
+                    user.setPassword(resultSet.getString("password"));
+                }
+                else {
+                    return null;
+                }
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return user;
+    }
+}

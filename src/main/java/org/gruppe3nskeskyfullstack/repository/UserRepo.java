aa73a4af7b36701e85f3d7cb791e1762fb33671c
jshar <joha0013@stud.ek.dk>
2026-04-15 15:37:46 +0200

    Login Controller - og userService begyndt

--- src/main/java/org/gruppe3nskeskyfullstack/repository/UserRepo.java
+++ src/main/java/org/gruppe3nskeskyfullstack/repository/UserRepo.java
@@ -1,4 +1,13 @@
 package org.gruppe3nskeskyfullstack.repository;
 
+import org.gruppe3nskeskyfullstack.model.User;
+
 public class UserRepo {
+
+
+    private User User;
+
+    public User findByEmailAndPassword(String email, String password){
+        return User;
+    }
 }

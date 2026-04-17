4b1f034bbc638f267091d65ead77cfa8b8c9dc93
Estera Turcu <estu0002@stud.ek.dk>
2026-04-17 15:16:30 +0200

    Wish service

---
+++ src/main/java/org/gruppe3nskeskyfullstack/service/WishService.java
@@ -0,0 +1,41 @@
+package org.gruppe3nskeskyfullstack.service;
+
+import org.gruppe3nskeskyfullstack.model.Wish;
+import org.gruppe3nskeskyfullstack.repository.WishRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service//opret et wishservice objekt
+public class WishService {
+    private final WishRepo wishRepo;
+
+    @Autowired//spring satrter app og finder @service klassen
+    //spring ser konstruktoren, finder en wishrepo bean og indsætter det i konstruktor
+    public WishService(WishRepo wishRepo){
+        this.wishRepo=wishRepo;
+    }
+
+    //metodesn sendes fra controller, userid videresendes. repo laver sqljoin. resultat fra db kommer i array. servise returnerer listen
+    public ArrayList<Wish>getAllWishesByUser(int userID){
+        return wishRepo.getAllWishesByUser(userID);
+    }
+
+    public void saveWish(Wish wish){
+        wishRepo.saveWish(wish);
+    }
+
+    public void deleteWish(int id){
+        wishRepo.deleteWish(id);
+    }
+
+    public Wish getWishByID(int id){
+        return wishRepo.getWishByID(id);
+    }
+
+    public void updateWish(Wish wish){
+        wishRepo.updateWish(wish);
+    }
+
+}

5954a97f94d55aaf68f07f77db974f43b1cc609f
Niko0004Stud <niko0004@stud.ek.dk>
2026-04-18 13:42:46 +0200

    Grundlæggende page til opretning af bruger

--- src/main/resources/dummy.sql
+++ src/main/resources/dummy.sql
@@ -2,5 +2,7 @@ USE WishDB;
 
 insert into wishlists (userID, name) VALUES
     ('1','lottes fødselsdag'),
-    ('1','bents fødseldag')
+    ('1','bents fødseldag');
 
+INSERT INTO users (firstName, lastName, email, tlfNumber, gender, birthDate, password)
+VALUES  ('Nicklas', 'Køppen', 'nicklasReal@email.dk', '12345667', 'mand', 08-02-2001, 'sikkerKode1');

3de29827131014131587b275d77aa6a2994e1bd0
Niko0004Stud <niko0004@stud.ek.dk>
2026-04-19 13:45:01 +0200

    Nu viser gaveobjekter sig på wishlistsiden for en given user

--- src/main/resources/databaseScript.sql
+++ src/main/resources/databaseScript.sql
@@ -22,10 +22,11 @@ CREATE TABLE users (
 
 
 CREATE TABLE wishlists (
-                           id INT AUTO_INCREMENT PRIMARY KEY,
-                           userID INT NOT NULL ,
-                           FOREIGN KEY (userID) REFERENCES users(id),
-                           name VARCHAR(20) NOT NULL
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            userID INT NOT NULL ,
+                            FOREIGN KEY (userID) REFERENCES users(id),
+                            name VARCHAR(20) NOT NULL,
+                            shareToken VARCHAR(255)
 );
 
 
@@ -39,16 +40,22 @@ CREATE TABLE wishes (
                         url VARCHAR(1000) NOT NULL
 );
 
-INSERT INTO users (firstName, lastName, email, tlfNumber, gender, birthDate, password)
-VALUES  ('Nicklas', 'Køppen', 'nicklasReal@email.dk', '12345667', 'mand', 20220208, 'sikkerKode1'),
-        ('Dummy', 'Haha', 'Fakemail@no.dk', '2020202', 'mand', 20010305, 'fakePassword');
+# INSERT INTO users (firstName, lastName, email, tlfNumber, gender, birthDate, password)
+# VALUES  ('Nicklas', 'Køppen', 'nicklasReal@email.dk', '12345667', 'mand', 20220208, 'sikkerKode1'),
+#         ('Dummy', 'Haha', 'Fakemail@no.dk', '2020202', 'mand', 20010305, 'fakePassword');
 
-insert into wishlists (userID, name)
-VALUES  ('1','lottes fødselsdag'),
-        ('1','bents fødseldag'),
-        ('2', 'Nicklas testWL')
-                                            ;
-ALTER TABLE wishlists ADD COLUMN shareToken VARCHAR(255);
+# insert into wishlists (userID, name)
+# VALUES  ('1','lottes fødselsdag'),
+#         ('1','bents fødseldag'),
+#         ('2', 'Nicklas testWL');
+
+# INSERT INTO wishes (wishlistID, name, price, url)
+# VALUES  ('1','Woody','100','noURL'),
+#         ('1','Buzz Lightyear','250','sillyURL'),
+#         ('2','Lego','1000','lego.dk'),
+#         ('3','Stick','10','nature');
+
+
 
 
 CREATE TABLE reservation (
